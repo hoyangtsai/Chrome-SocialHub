e3b83e52c7eba91edf3e028d8ce6b7ce8fb38afb
Ho-Yang Tsai <hoyang.t@gmail.com>
2015-08-10 12:30:54 +0800

    single mode view without material design lite integration

--- background.js
+++ background.js
@@ -2,7 +2,7 @@ chrome.app.runtime.onLaunched.addListener(function() {
   chrome.app.window.create('default.html', {
     'bounds': {
       'width': 1140,
-      'height': 780
+      'height': 760
     }
   });
 });
--- launch.js
+++ launch.js
@@ -1,7 +1,5 @@
 var App = {
   webview: null,
-  viewSwitch: null,
-  layoutTitle: null,
 
   init: function() {
     this.webview = document.getElementById('the_webview');
@@ -21,12 +19,6 @@ var App = {
     });
 
     window.addEventListener('keydown', this.keydownHandler);
-
-    this.viewSwitch = document.getElementById('switch');
-    this.viewSwitch.addEventListener('click', this.handleSwitchView);
-
-    this.layoutTitle = document.getElementById('layout-title');
-    this.layoutTitle.addEventListener('click', this.handleLayoutTitle);
   },
 
   keydownHandler: function(event) {
@@ -41,29 +33,7 @@ var App = {
           break;
       }
     }
-  },
-
-  handleSwitchView: function(event) {
-    event.preventDefault();
-    if (this.dataset.mode == 'desktop') {
-      App.webview.src = 'https://m.facebook.com/';
-      this.dataset.mode = 'mobile';
-      this.innerHTML = 'Desktop';
-    } else if (this.dataset.mode == 'mobile') {
-      App.webview.src = 'https://www.facebook.com/';
-      this.dataset.mode = 'desktop';
-      this.innerHTML = 'Mobile';
-    }
-  },
-
-  handleLayoutTitle: function(event) {
-    if (App.viewSwitch.dataset.mode == 'desktop') {
-      App.webview.src = 'https://www.facebook.com/';
-    } else if (App.viewSwitch.dataset.mode == 'mobile') {
-      App.webview.src = 'https://m.facebook.com/';
-    }
   }
-
 };
 
 window.addEventListener('load', App.init.bind(App));
