aadcdeb49b3191e5fadcf2ee1172eb4467ff5eff
Ho-Yang Tsai <hoyang.t@gmail.com>
2015-08-24 14:01:50 +0800

    Upgrade material design lite to 1.0.4

--- background.js
+++ background.js
@@ -1,8 +1,8 @@
 chrome.app.runtime.onLaunched.addListener(function() {
   chrome.app.window.create('default.html', {
     'bounds': {
-      'width': 1128,
-      'height': 803
+      'width': 1280,
+      'height': 800
     }
   });
 });
--- launch.js
+++ launch.js
@@ -1,8 +1,5 @@
 var App = {
   webview: null,
-  layoutTitle: null,
-  gotoFacebook: null,
-  gotoInstagram: null,
 
   init: function() {
     this.webview = document.getElementById('the_webview');
@@ -14,23 +11,18 @@ var App = {
     });
 
     this.webview.addEventListener('permissionrequest', function(e) {
-      if (e.permission === 'fullscreen' ||
-          e.permission === 'download' ||
-          e.permission === 'geolocation') {
+      if (e.permission === 'fullscreen' || e.permission === 'download' ||
+          e.permission === 'geolocation' || e.permission === 'filesystem') {
         e.request.allow();
       }
     });
 
     window.addEventListener('keydown', this.keydownHandler);
 
-    this.layoutTitle = document.getElementById('layout-title');
-    // this.layoutTitle.addEventListener('click', this.handleLayoutTitle);
-
-    this.gotoFacebook = document.getElementById('goto-facebook');
-    this.gotoFacebook.addEventListener('click', this.handleGoto);
-
-    this.gotoInstagram = document.getElementById('goto-instagram');
-    this.gotoInstagram.addEventListener('click', this.handleGoto);
+    var navLinks = document.getElementsByClassName('mdl-navigation__link');
+    for (var i = 0; i < navLinks.length; i++) {
+      navLinks[i].addEventListener('click', this.handleGoto);
+    }
   },
 
   keydownHandler: function(event) {
@@ -47,33 +39,14 @@ var App = {
     }
   },
 
-  handleSwitchView: function(event) {
-    event.preventDefault();
-    if (this.dataset.mode == 'desktop') {
-      App.webview.src = 'https://m.facebook.com/';
-      this.dataset.mode = 'mobile';
-      this.innerHTML = 'Desktop';
-    } else if (this.dataset.mode == 'mobile') {
-      App.webview.src = 'https://www.facebook.com/';
-      this.dataset.mode = 'desktop';
-      this.innerHTML = 'Mobile';
-    }
-  },
-
-  handleLayoutTitle: function(event) {
-    if (App.viewSwitch.dataset.mode == 'desktop') {
-      App.webview.src = 'https://www.facebook.com/';
-    } else if (App.viewSwitch.dataset.mode == 'mobile') {
-      App.webview.src = 'https://m.facebook.com/';
-    }
-  },
-
   handleGoto: function(event) {
     event.preventDefault();
     var anchor = event.target;
     App.layoutTitle.innerHTML = anchor.innerHTML;
     App.webview.src = anchor.href;
+    return false;
   }
+
 };
 
 window.addEventListener('load', App.init.bind(App));
